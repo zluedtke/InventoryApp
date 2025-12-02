10f2361d012d88f07f4bde2b2088fbf6251f7a2b
zluedtke <zluedtke@rrc.ca>
2025-12-01 18:35:21 -0600

    Update lambda_function.py

--- lambda/get_inventory_item/lambda_function.py
+++ lambda/get_inventory_item/lambda_function.py
@@ -1,6 +1,17 @@
 import boto3
 import json
 
+
+# Function to convert Decimal to int/float
+def convert_decimals(obj):
+    if isinstance(obj, list):
+        return [convert_decimals(i) for i in obj]
+    elif isinstance(obj, dict):
+        return {k: convert_decimals(v) for k, v in obj.items()}
+    elif isinstance(obj, Decimal):  
+        return int(obj) if obj % 1 == 0 else float(obj)  # Convert to int if whole number, else float
+    return obj
+
 def lambda_handler(event, context):
     # DynamoDB setup
     dynamo_client = boto3.client('dynamodb')
@@ -30,14 +41,19 @@ def lambda_handler(event, context):
                 'statusCode': 404,
                 'body': json.dumps('Item not found')
             }
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps(item, default=str)  # Use str to handle any special types like Decimal
-        }
+        items = convert_decimals(items)
+        
     except Exception as e:
         print(e)
         return {
             'statusCode': 500,
             'body': json.dumps(str(e))
+
+        }
+
+
+
+    return {
+            'statusCode': 200,
+            'body': json.dumps(item)
         }
