02f5c98929c1e8ee530e296fe9559eff0e39f696
zluedtke <zluedtke@rrc.ca>
2025-12-01 22:39:16 -0600

    Update lambda_function.py

--- lambda/get_inventory_item/lambda_function.py
+++ lambda/get_inventory_item/lambda_function.py
@@ -1,21 +1,23 @@
 import boto3
 import json
+from decimal import Decimal
+from boto3.dynamodb.conditions import Key
+
+dynamodb = boto3.resource('dynamodb')
 
 
-# Function to convert Decimal to int/float
-def convert_decimals(obj):
+def convert_decimal(obj):
     if isinstance(obj, list):
-        return [convert_decimals(i) for i in obj]
-    elif isinstance(obj, dict):
-        return {k: convert_decimals(v) for k, v in obj.items()}
-    elif isinstance(obj, Decimal):  
-        return int(obj) if obj % 1 == 0 else float(obj)  # Convert to int if whole number, else float
+        return [convert_decimal(i) for i in obj]
+    if isinstance(obj, dict):
+        return {k: convert_decimal(v) for k, v in obj.items()}
+    if isinstance(obj, Decimal):
+        return int(obj) if obj % 1 == 0 else float(obj)
     return obj
 
+
 def lambda_handler(event, context):
-    # DynamoDB setup
-    dynamo_client = boto3.client('dynamodb')
-    table_name = 'Inventory'
+    table = dynamodb.Table('Inventory')
 
     # Get the key from the path parameters
     if 'pathParameters' not in event or 'id' not in event['pathParameters']:
@@ -26,22 +28,22 @@ def lambda_handler(event, context):
 
     key_value = event['pathParameters']['id']
 
-    # Prepare the key for DynamoDB
-    key = {
-        'id': {'S': key_value}
-    }
-
     # Get the item from the table
     try:
-        response = dynamo_client.get_item(TableName=table_name, Key=key)
-        item = response.get('Item', {})
+        response = table.query(
+            KeyConditionExpression=Key('_id').eq(key_value)
+        )
 
-        if not item:
+        items = response.get('Items', [])
+
+
+        if not items:
             return {
                 'statusCode': 404,
                 'body': json.dumps('Item not found')
             }
-        items = convert_decimals(items)
+        
+        items = convert_decimal(items)
         
     except Exception as e:
         print(e)
@@ -55,5 +57,5 @@ def lambda_handler(event, context):
 
     return {
             'statusCode': 200,
-            'body': json.dumps(item)
+            'body': json.dumps(items)
         }
